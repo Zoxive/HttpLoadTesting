c40896c1cdb61ca22e603a6c12edd1898f77d028
Mike Heskamp <Mike.Heskamp@infor.com>
2017-01-12 13:28:51 -0500

    Add support for null query string values for method and requestUrl

--- src/Zoxive.HttpLoadTesting.Client/Domain/HttpStatusResult/Repositories/HttpStatusResultRepository.cs
+++ src/Zoxive.HttpLoadTesting.Client/Domain/HttpStatusResult/Repositories/HttpStatusResultRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
@@ -17,32 +18,86 @@ namespace Zoxive.HttpLoadTesting.Client.Domain.HttpStatusResult.Repositories
 
         public async Task<string[]> GetDistinctRequestUrls(string method)
         {
-            var requestUrls =
-                await
-                    _dbConnection.QueryAsync<string>(
-                        "SELECT DISTINCT RequestUrl FROM HttpStatusResult WHERE Method = @method", new {method});
+            var sqlParams = new Dictionary<string, object>();
+
+            var sql = "SELECT DISTINCT RequestUrl FROM HttpStatusResult";
+
+            if (!string.IsNullOrEmpty(method))
+            {
+                sql += " WHERE Method = @method";
+                sqlParams.Add("method", method);
+            }
+
+            var requestUrls = await _dbConnection.QueryAsync<string>(sql, sqlParams);
 
             return requestUrls.ToArray();
         }
 
         public async Task<string[]> GetDistinctMethods(string requestUrl)
         {
-            var methods =
-                await
-                    _dbConnection.QueryAsync<string>(
-                        "SELECT DISTINCT Method FROM HttpStatusResult WHERE RequestUrl = @requestUrl", new {requestUrl});
+            var sqlParams = new Dictionary<string, object>();
+
+            var sql = "SELECT DISTINCT Method FROM HttpStatusResult";
+
+            if (!string.IsNullOrEmpty(requestUrl))
+            {
+                sql += " WHERE RequestUrl = @requestUrl";
+                sqlParams.Add("requestUrl", requestUrl);
+            }
+
+            var methods = await _dbConnection.QueryAsync<string>(sql, sqlParams);
 
             return methods.ToArray();
         }
 
         public async Task<HttpStatusResultStatistics> GetStatistics(string method, string requestUrl)
         {
-            var averageDuration =
-                await
-                    _dbConnection.ExecuteScalarAsync<long>(
-                        "SELECT AVG(ElapsedMilliseconds) AS AverageDuration FROM HttpStatusResult");
+            var sql = "SELECT ElapsedMilliseconds FROM HttpStatusResult";
+
+            IDictionary<string, object> sqlParams;
+            var whereClause = CreateWhereClause(method, requestUrl, out sqlParams);
+
+            sql += whereClause;
+
+            var averageDuration = await _dbConnection.ExecuteScalarAsync<long>(sql, sqlParams);
 
             return new HttpStatusResultStatistics(method, requestUrl, averageDuration);
+        }
+
+        private static string CreateWhereClause(string method, string requestUrl, out IDictionary<string, object> sqlParams)
+        {
+            sqlParams = new Dictionary<string, object>();
+
+            var whereCriteria = new List<string>();
+            if (!string.IsNullOrEmpty(method))
+            {
+                whereCriteria.Add("Method = @method");
+                sqlParams.Add("method", method);
+            }
+
+            if (!string.IsNullOrEmpty(requestUrl))
+            {
+                whereCriteria.Add("RequestUrl = @requestUrl");
+                sqlParams.Add("requestUrl", requestUrl);
+            }
+
+            if (whereCriteria.Count == 0)
+                return "";
+
+            var whereClause = " WHERE ";
+            for(var i = 0; i < whereCriteria.Count; i++)
+            {
+                if (i == 0)
+                {
+                    whereClause += whereCriteria[i];
+                }
+                else
+                {
+                    whereClause += " AND " + whereCriteria[i];
+                }
+            }
+
+            return whereClause;
         }
     }
 }
