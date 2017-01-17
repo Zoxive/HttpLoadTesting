460773bca2fcdcbf7d978b83cac63b2f6df3e5a5
Kyle Wascher <kyle@zoxive.com>
2017-01-17 15:57:31 -0500

    0.0.7

--- createpackages.bat
+++ createpackages.bat
@@ -1,5 +1,9 @@
 del *.nupkg
 
+cd /d src\Zoxive.HttpLoadTesting.Client
+CMD /C npm run release
+cd /d ../..
+
 c:\Utilities\nuget.exe pack src\Zoxive.HttpLoadTesting.Client\project.json
 c:\Utilities\nuget.exe pack src\Zoxive.HttpLoadTesting.Framework\project.json
 
