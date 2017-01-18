efb9c914978d938a10191b78e8ace442df467849
Kyle Wascher <kylewascher@gmail.com>
2017-01-18 14:42:09 -0500

    Remove old proj files

--- createpackages.bat
+++ createpackages.bat
@@ -1,12 +1,12 @@
-del *.nupkg
+del nupkgs\*.nupkg
 
-REM cd /d src\Zoxive.HttpLoadTesting.Client
-REM CMD /C npm run release
-REM cd /d ../..
+cd /d src\Zoxive.HttpLoadTesting.Client
+CMD /C npm run release
+cd /d ../..
 
 dotnet restore
 
-dotnet pack src\Zoxive.HttpLoadTesting.Framework\Zoxive.HttpLoadTesting.Framework.csproj--configuration release
-dotnet pack src\Zoxive.HttpLoadTesting.Client\Zoxive.HttpLoadTesting.Client.csproj --configuration release
+dotnet pack src\Zoxive.HttpLoadTesting.Framework\Zoxive.HttpLoadTesting.Framework.csproj --configuration release --output ..\..\nupkgs
+dotnet pack src\Zoxive.HttpLoadTesting.Client\Zoxive.HttpLoadTesting.Client.csproj --configuration release --output ..\..\nupkgs
 
 REM c:\Utilities\nuget.exe push *.nupkg -source "https://api.nuget.org/v3/index.json"
