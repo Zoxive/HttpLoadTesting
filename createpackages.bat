133dfd318a337432790118d47ae9633a4b67231f
Kyle Wascher <kylewascher@gmail.com>
2017-01-20 13:06:34 -0500

    bump to 0.0.8

--- createpackages.bat
+++ createpackages.bat
@@ -9,4 +9,4 @@ dotnet restore
 dotnet pack src\Zoxive.HttpLoadTesting.Framework\Zoxive.HttpLoadTesting.Framework.csproj --configuration release --output ..\..\nupkgs
 dotnet pack src\Zoxive.HttpLoadTesting.Client\Zoxive.HttpLoadTesting.Client.csproj --configuration release --output ..\..\nupkgs
 
-REM c:\Utilities\nuget.exe push *.nupkg -source "https://api.nuget.org/v3/index.json"
+c:\Utilities\nuget.exe push nupkgs\*.nupkg -source "https://api.nuget.org/v3/index.json"
