11ba740f0fbe79996475d8bf07a2dff91a0aeebb
Kyle Wascher <Kyle.Wascher@infor.com>
2017-07-20 18:53:39 -0400

    got gaps to show

--- src/Zoxive.HttpLoadTesting.Client/scripts/linegraph/store/graphStatsReducer.ts
+++ src/Zoxive.HttpLoadTesting.Client/scripts/linegraph/store/graphStatsReducer.ts
@@ -21,9 +21,40 @@ export default function graphStatsReducer(state: GraphStatsState, action: IActio
     switch (action.type)
     {
         case RECIEVE_GRAPH_STATS:
-            const graphStats = action as IActionGraphStats;
-            return xtend(state, { stats: graphStats.graphStats, groupSize: graphStats.groupSize });
+            const { groupSize, graphStats } = action as IActionGraphStats;
+
+            const graphStatsMap = new Map<number, GraphStat>
+            (
+                graphStats.map(x => [x.grp, x] as [number, GraphStat])
+            );
+
+            for (let i = 0; i < groupSize; i++)
+            {
+                graphStatsMap.set(i, getOrCreateEmptyGraphStat(graphStatsMap.get(i), i));
+            }
+
+            // to array and sort by grp
+            const arr = Array.from(graphStatsMap.values())
+                .sort((a, b) => a.grp - b.grp);
+
+            return xtend(state, { stats: arr, groupSize: groupSize });
     }
 
     return state;
+}
+
+function getOrCreateEmptyGraphStat(stat: GraphStat, grp: number): GraphStat
+{
+    if (stat) return stat;
+
+    return {
+        grp: grp,
+        requests: 0,
+        users: 0,
+        avg: 0,
+        min: 0,
+        max: 0,
+        variance: 0,
+        std: 0
+    };
 }
