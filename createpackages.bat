b79cafaab84d3355bd06959f5ccb3771259dcd9b
Kyle Wascher <Kyle.Wascher@infor.com>
2019-10-23 09:07:37 -0400

    fix create packages

--- createpackages.bat
+++ createpackages.bat
@@ -2,6 +2,6 @@ del nupkgs\*.nupkg
 
 dotnet restore
 
-dotnet pack src\Zoxive.HttpLoadTesting.Client\Zoxive.HttpLoadTesting.Client.csproj --configuration release --output ..\..\nupkgs
+dotnet pack src\Zoxive.HttpLoadTesting.Client\Zoxive.HttpLoadTesting.Client.csproj --configuration release --output nupkgs
 
 c:\Utilities\nuget.exe push nupkgs\*.nupkg -source "https://api.nuget.org/v3/index.json"
