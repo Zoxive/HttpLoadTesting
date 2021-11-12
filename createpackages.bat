96a33c7da5368b25005416f6959a19388c1da1d4
Kyle Wascher <Kyle.Wascher@infor.com>
2021-11-12 06:52:55 -0500

    f

--- createpackages.bat
+++ createpackages.bat
@@ -4,4 +4,4 @@ dotnet restore
 
 dotnet pack src\Zoxive.HttpLoadTesting.Client\Zoxive.HttpLoadTesting.Client.csproj --configuration release --output nupkgs
 
-c:\Utilities\nuget.exe push nupkgs\*.nupkg -source "https://api.nuget.org/v3/index.json"
+f:\Utilities\nuget.exe push nupkgs\*.nupkg -source "https://api.nuget.org/v3/index.json"
